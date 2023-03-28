50e43ecfe74dc7749071a88afcbe8a9f21c7651f
Gautam Kumarm <g2gautamsingh@gmail.com>
2023-03-28 05:47:09 +0530

    done

--- frontend/src/component/DashBoard.jsx
+++ frontend/src/component/DashBoard.jsx
@@ -2,14 +2,59 @@ import React, { useState } from "react";
 import "../App.css";
 
 const DashBoard = () => {
-  const data = new Array(15).fill(0);
-  const [input, setInput] = useState("");
+  const [activeCell, setActiveCell] = useState(null);
+  const [data, setData] = useState([
+    ["", "", "", "", ""],
+    ["", "", "", "", ""],
+    ["", "", "", "", ""],
+    ["", "", "", "", ""],
+    ["", "", "", "", ""],
+  ]);
+
+  const handleChange = (e) => {
+    const newData = [...data];
+    newData[activeCell.rowIndex][activeCell.colIndex] = e.target.value;
+    setData(newData);
+  };
+
+  function handleClick(rowIndex, colIndex) {
+    setActiveCell({ rowIndex, colIndex });
+  }
+
+  function convertableCell(rowIndex, colIndex, value) {
+    if (
+      activeCell &&
+      activeCell.rowIndex === rowIndex &&
+      activeCell.colIndex === colIndex
+    ) {
+      return (
+        <input
+          type="text"
+          className="input"
+          value={value}
+          onChange={handleChange}
+          onBlur={() => setActiveCell(null)}
+          autoFocus
+        />
+      );
+    } else {
+      return (
+        <div className="cell" onClick={() => handleClick(rowIndex, colIndex)}>
+          {value}
+        </div>
+      );
+    }
+  }
 
   return (
     <div className="container">
-      {data.map((el) => (
-        <div className="box">
-          <input type="text" />
+      {data.map((row, rowIndex) => (
+        <div className="box" key={rowIndex}>
+          {row.map((cell, colIndex) => (
+            <div className="column" key={colIndex}>
+              {convertableCell(rowIndex, colIndex, cell)}
+            </div>
+          ))}
         </div>
       ))}
     </div>
